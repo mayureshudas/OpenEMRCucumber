a4a745482e7c42a91f67db8adaa7cd627d1db230
mayureshudas <mayuresh.udas@capgemini.com>
2016-08-19 16:23:32 +0530

    code

--- src/stepRunner/StepRunner.java
+++ src/stepRunner/StepRunner.java
@@ -15,10 +15,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+
 
 public class StepRunner {
 	
